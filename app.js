9debd756d65a600babfaec4d27a8d3de7d60cb45
aryangupta701 <garyan447@gmail.com>
2022-07-18 19:25:50 +0530

    local

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(fileUpload())
 app.use(cors({
-    origin: 'http://localhost:3000'
+    withCredentials: true ,
+    origin: process.env.NODE_ENV !== "PRODUCTION" ? 'http://localhost:3000' : 'https://ecommerce-frontend-appio.herokuapp.com'
 }))
 const errorMiddleware = require('./middlewares/error')
 const mongoose = require('mongoose')
--- util/jwtToken.js
+++ util/jwtToken.js
@@ -9,7 +9,8 @@ const jwttoken = (user,statusCode, res)=>{
         expires : new Date( 
             Date.now() + process.env.COOKIE_EXPIRE*24*60*60*1000
         ),
-        httpsOnly : true
+        httpOnly : true,
+        secure : process.env.NODE_ENV === "PRODUCTION"
     }
     res.status(statusCode).cookie("token", token , options).json({
         success : true, 
