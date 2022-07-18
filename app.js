0bbe6b6acf965679674d69daba055c9aa5470df1
aryangupta701 <garyan447@gmail.com>
2022-07-18 22:45:08 +0530

    github add

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cookieParser())
 app.use(fileUpload())
 app.use(cors({
     credentials: true ,
-    origin: process.env.NODE_ENV !== "PRODUCTION" ? 'http://localhost:3000' : 'https://ecommerce-frontend-appio.herokuapp.com'
+    origin: process.env.NODE_ENV !== "PRODUCTION" ? 'http://localhost:3000' : 'https://aryangupta701.github.io/'
 }))
 const errorMiddleware = require('./middlewares/error')
 const mongoose = require('mongoose')
